3a1f3531184b19615580e9290cb33c3c5fbef14a
siyatut <anastasiya.tyutinova@gmail.com>
2024-12-23 11:29:49 +0300

    Separate create layout from HabitsViewController

--- bebrave/HabitsViewController/HabitsViewController.swift
+++ bebrave/HabitsViewController/HabitsViewController.swift
@@ -38,92 +38,11 @@ class HabitsViewController: UICollectionViewController {
 // MARK: — Init
     
     init() {
-        super.init(collectionViewLayout: HabitsViewController.createLayout())
+        super.init(collectionViewLayout: HabitsLayout.createLayout())
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-// MARK: - Create layout
-    
-    static func createLayout() -> UICollectionViewLayout {
-        let provider: UICollectionViewCompositionalLayoutSectionProvider = { section, environment in
-
-            if section == 0 {
-                let item = NSCollectionLayoutItem(
-                    layoutSize: .init(
-                        widthDimension: .fractionalWidth(1),
-                        heightDimension: .fractionalHeight(1)
-                    )
-                )
-                let group = NSCollectionLayoutGroup.vertical(
-                    layoutSize: .init(
-                        widthDimension: .fractionalWidth(1),
-                        heightDimension: .absolute(312)
-                    ),
-                    subitems: [item]
-                )
-                let section = NSCollectionLayoutSection(group: group)
-                section.contentInsets = .init(top: 12, leading: 12, bottom: 0, trailing: 12)
-                return section
-            }
-            
-            let background = NSCollectionLayoutSupplementaryItem(
-                layoutSize: .init(
-                    widthDimension: .fractionalWidth(1),
-                    heightDimension: .fractionalHeight(1)
-                ),
-                elementKind: CustomElement.outlineBackground.rawValue,
-                containerAnchor: .init(edges: .all)
-            )
-            let item = NSCollectionLayoutItem(
-                layoutSize: .init(
-                    widthDimension: .fractionalWidth(1),
-                    heightDimension: .absolute(60)
-                ),
-                supplementaryItems: [background]
-            )
-            let group = NSCollectionLayoutGroup.vertical(
-                layoutSize: .init(
-                    widthDimension: .fractionalWidth(1),
-                    heightDimension: .absolute(60)
-                ),
-                subitems: [item]
-            )
-            
-            let section = NSCollectionLayoutSection(group: group)
-            section.contentInsets = .init(top: 0, leading: 12, bottom: 0, trailing: 12)
-            section.interGroupSpacing = 8
-            return section
-        }
-        let header = NSCollectionLayoutBoundarySupplementaryItem(
-            layoutSize: .init(
-                widthDimension: .fractionalWidth(1),
-                heightDimension: .absolute(70)
-            ),
-            elementKind: CustomElement.collectionHeader.rawValue,
-            alignment: .top,
-            absoluteOffset: .init(x: 0, y: -24)
-        )
-        header.extendsBoundary = true
-        header.contentInsets = .init(top: 0, leading: 12, bottom: 0, trailing: 12)
-        
-        let footer = NSCollectionLayoutBoundarySupplementaryItem(
-            layoutSize: .init(
-                widthDimension: .fractionalWidth(1),
-                heightDimension: .absolute(48)
-            ),
-            elementKind: CustomElement.collectionFooter.rawValue,
-            alignment: .bottom,
-            absoluteOffset: .init(x: 0, y: 24)
-        )
-        footer.extendsBoundary = true
-        footer.contentInsets = .init(top: 0, leading: 12, bottom: 0, trailing: 12)
-        
-        let configuration = UICollectionViewCompositionalLayoutConfiguration()
-        configuration.boundarySupplementaryItems = [header, footer]
-        return UICollectionViewCompositionalLayout(sectionProvider: provider, configuration: configuration)
     }
     
 // MARK: - Lifecycle view
