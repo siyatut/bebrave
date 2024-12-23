89cb5a63bb4474162cd09a2bd43e54b7080be7f7
siyatut <anastasiya.tyutinova@gmail.com>
2024-12-23 10:39:28 +0300

    Reorganise files in project

--- bebrave/HabitsViewController/HabitsViewController.swift
+++ bebrave/HabitsViewController/HabitsViewController.swift
@@ -194,7 +194,7 @@ class HabitsViewController: UICollectionViewController {
 extension HabitsViewController {
     
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        let sections = habits.isEmpty ? 1 : 2
+        let sections = habits.isEmpty ? 1 : 2 // здесь 2, потому что возвращаются обе секции, но если нет привычек, itemCount = 0
         print("Calculating number of sections. Habits count: \(habits.count). Returning sections: \(sections)")
         return sections
     }
