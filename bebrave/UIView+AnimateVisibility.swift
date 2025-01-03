d63aa87e1c390a1cc984eedc9fba0abf18161e3c
siyatut <anastasiya.tyutinova@gmail.com>
2025-01-03 09:38:18 +0300

    Update animate visibility

--- bebrave/UIView+AnimateVisibility.swift
+++ bebrave/UIView+AnimateVisibility.swift
@@ -8,21 +8,21 @@
 import UIKit
 
 extension UIView {
-    func animateVisibility(isVisible: Bool, duration: TimeInterval = 0.4, transformEffect: Bool = false, completion: (() -> Void)? = nil) {
+    func animateVisibility(isVisible: Bool, duration: TimeInterval = 0.8, transformEffect: Bool = false, completion: (() -> Void)? = nil) {
         if isVisible {
             self.alpha = 0
             self.isHidden = false
             
             if transformEffect {
-                self.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
+                self.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
             }
             
             UIView.animate(
                 withDuration: duration,
                 delay: 0,
-                usingSpringWithDamping: transformEffect ? 0.8 : 1.0,
-                initialSpringVelocity: transformEffect ? 0.5 : 0.0,
-                options: [],
+                usingSpringWithDamping: transformEffect ? 0.9 : 1.0,
+                initialSpringVelocity: transformEffect ? 0.3 : 0.0,
+                options: [.curveEaseInOut],
                 animations: {
                     self.alpha = 1
                     if transformEffect {
@@ -34,15 +34,20 @@ extension UIView {
                 }
             )
         } else {
-            UIView.animate(withDuration: duration, animations: {
-                self.alpha = 0
-            }, completion: { _ in
-                self.isHidden = true
-                if transformEffect {
-                    self.transform = .identity
-                }
-                completion?()
-            })
+            UIView.animate(
+                withDuration: duration * 0.8,
+                animations: {
+                    self.alpha = 0
+                },
+                completion: { _ in
+                    self.isHidden = true
+                    if transformEffect {
+                        self.transform = .identity
+                    }
+                    completion?()
+                })
         }
     }
 }
+
+#warning("Подумать, может быть, вообще добавить emptyStateView to NewHabitVC, чтобы при добавлении первой привычки ещё до закрытия экрана показывать emptyStateView")
