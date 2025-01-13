8844967712a7daa19bdfdf5cf9304153215ce8c1
siyatut <anastasiya.tyutinova@gmail.com>
2025-01-13 22:12:42 +0300

    Separate HabitCell+Layout

---
+++ bebrave/ViewControllers/Habits/HabitsViewController/Cells/HabitCell/HabitCell+Layout.swift
@@ -0,0 +1,113 @@
+//
+//  HabitCell+Layout.swift
+//  bebrave
+//
+//  Created by Anastasia Tyutinova on 13/1/2568 BE.
+//
+
+import UIKit
+
+extension HabitCell {
+    
+    func addSubviewsToStackView(_ stackView: UIStackView, views: [UIView]) {
+        views.forEach { stackView.addArrangedSubview($0) }
+    }
+    
+    func setupComponents() {
+        
+        contentView.addSubview(rightButtonContainer)
+        contentView.addSubview(leftButtonContainer)
+        contentView.addSubview(contentContainer)
+        
+        NSLayoutConstraint.activate([
+            leftButtonContainer.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            leftButtonContainer.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            leftButtonContainer.topAnchor.constraint(equalTo: contentView.topAnchor),
+            leftButtonContainer.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            leftButtonContainer.widthAnchor.constraint(equalToConstant: buttonWidth * 2)
+        ])
+        
+        NSLayoutConstraint.activate([
+            rightButtonContainer.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            rightButtonContainer.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            rightButtonContainer.topAnchor.constraint(equalTo: contentView.topAnchor),
+            rightButtonContainer.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            rightButtonContainer.widthAnchor.constraint(equalToConstant: buttonWidth * 2)
+        ])
+        
+        NSLayoutConstraint.activate([
+            contentContainer.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            contentContainer.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            contentContainer.topAnchor.constraint(equalTo: contentView.topAnchor),
+            contentContainer.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+        
+        // По-моему, вот эти 4 строчки снизу можно удалить
+        
+        contentView.clipsToBounds = false
+        leftButtonContainer.clipsToBounds = false
+        rightButtonContainer.clipsToBounds = false
+        contentContainer.clipsToBounds = true
+
+        contentContainer.addSubview(progressView)
+        progressViewWidthConstraint = progressView.widthAnchor.constraint(equalToConstant: 0)
+        
+        NSLayoutConstraint.activate([
+            progressView.leadingAnchor.constraint(equalTo: contentContainer.leadingAnchor),
+            progressView.topAnchor.constraint(equalTo: contentContainer.topAnchor),
+            progressView.bottomAnchor.constraint(equalTo: contentContainer.bottomAnchor),
+            progressViewWidthConstraint
+        ])
+        
+        contentContainer.addSubview(horizontalStackView)
+        contentContainer.addSubview(percentDone)
+        contentContainer.addSubview(checkbox)
+        
+        addSubviewsToStackView(horizontalStackView, views: [habitsName, starDivider, habitsCount])
+        
+        NSLayoutConstraint.activate([
+            horizontalStackView.leadingAnchor.constraint(equalTo: contentContainer.leadingAnchor, constant: 16),
+            horizontalStackView.topAnchor.constraint(equalTo: contentContainer.topAnchor, constant: 10),
+            horizontalStackView.trailingAnchor.constraint(lessThanOrEqualTo: checkbox.leadingAnchor, constant: 106),
+            
+            percentDone.topAnchor.constraint(equalTo: horizontalStackView.bottomAnchor, constant: 4),
+            percentDone.bottomAnchor.constraint(equalTo: contentContainer.bottomAnchor, constant: -12),
+            percentDone.leadingAnchor.constraint(equalTo: horizontalStackView.leadingAnchor),
+            percentDone.trailingAnchor.constraint(lessThanOrEqualTo: checkbox.leadingAnchor, constant: 106),
+            
+            checkbox.trailingAnchor.constraint(equalTo: contentContainer.trailingAnchor, constant: -23),
+            checkbox.centerYAnchor.constraint(equalTo: contentContainer.centerYAnchor),
+            checkbox.heightAnchor.constraint(equalToConstant: 20),
+            checkbox.widthAnchor.constraint(equalToConstant: 20)
+        ])
+        
+        leftButtonContainer.isHidden = false
+        rightButtonContainer.isHidden = false
+        
+        setupButtonConstraints()
+    }
+    
+   func setupButtonConstraints() {
+        let rightButtons = [deleteButton, skipButton]
+        for (index, button) in rightButtons.enumerated() {
+            rightButtonContainer.addSubview(button)
+            NSLayoutConstraint.activate([
+                button.trailingAnchor.constraint(equalTo: rightButtonContainer.trailingAnchor, constant: -CGFloat(index) * buttonWidth),
+                button.topAnchor.constraint(equalTo: rightButtonContainer.topAnchor),
+                button.bottomAnchor.constraint(equalTo: rightButtonContainer.bottomAnchor),
+                button.widthAnchor.constraint(equalToConstant: buttonWidth)
+            ])
+        }
+        
+        let leftButtons = [editButton, cancelButton]
+        for (index, button) in leftButtons.enumerated() {
+            leftButtonContainer.addSubview(button)
+            NSLayoutConstraint.activate([
+                button.leadingAnchor.constraint(equalTo: leftButtonContainer.leadingAnchor, constant: CGFloat(index) * buttonWidth),
+                button.topAnchor.constraint(equalTo: leftButtonContainer.topAnchor),
+                button.bottomAnchor.constraint(equalTo: leftButtonContainer.bottomAnchor),
+                button.widthAnchor.constraint(equalToConstant: buttonWidth)
+            ])
+        }
+    }
+}
